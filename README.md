3b919cb7bd733bbea7242672785c6a495e5363c1
Marcela Mallett <marcelamallett@Marcelas-MacBook-Air.local>
2020-04-04 15:19:18 -0500

    creating a simple mutation

--- README.md
+++ README.md
@@ -3,3 +3,26 @@
 ### To The Start Server
 - Run `npm start`
 - Server will run locally on port 4000
+
+### A basic query to get a list of Links:
+```
+query {
+  feed {
+    id
+    description
+    url
+  }
+}
+```
+
+### A basic mutation to create a new Link:
+```
+mutation {
+  post(
+    url: "www.prisma.io",
+    description: "prisma replaces traditional ORMs"
+  ) {
+    id
+  }
+}
+```
--- src/index.js
+++ src/index.js
@@ -1,19 +1,5 @@
 const {GraphQLServer} = require('graphql-yoga');
 
-// GraphQL Schema
-const typeDefs = `
-    type Query {
-        info: String!
-        feed: [Link!]!
-    }
-
-    type Link {
-        id: ID!
-        description: String!
-        url: String!
-    }
-`;
-
 // Dummy Data
 let links = [{
     id: 'link-0',
@@ -21,21 +7,38 @@ let links = [{
     description: 'Fullstack tutorial for GraphQL'
   }];
 
+let idCount = links.length;
+
 // Implementation of the Schema
 const resolvers = {
     Query: {
         info: () => `This is the API of Hackernews Clone`,
         feed: () => links
     },
-    Link: {
-        id: (parent) => parent.id,
-        description: (parent) => parent.description,
-        url: (parent) => parent.url
+    Mutation: {
+        post: (parent, args) => {
+            const link = {
+                id: `link-${idCount++}`,
+                description: args.description,
+                url: args.url
+            };
+
+            links.push(link);
+
+            return link;
+        }
     }
+    
+    // Can remove the Link. GraphQL infers what this looks like
+    // Link: {
+    //     id: (parent) => parent.id,
+    //     description: (parent) => parent.description,
+    //     url: (parent) => parent.url
+    // }
 };
 
 const server = new GraphQLServer({
-    typeDefs,
+    typeDefs: './src/schema.graphql',
     resolvers
 });
 
