466ea2cefa571d17646d7739de2de1287a506807
Marcela Mallett <marcelamallett@Marcelas-MacBook-Air.local>
2020-04-11 16:33:45 -0500

    getIsAuthorized function

--- src/utils.js
+++ src/utils.js
@@ -1,20 +1,30 @@
 const jwt = require('jsonwebtoken');
 const APP_SECRET = '9425196B-3B6A-4F7F-85E9-45F30839E41C';
 
-function getUserId(context) {
-    const Authorization = context.request.get('Authorization');
+function getIsAuthorized({request}) {
+    const Authorization = request.get('Authorization');
 
     if (Authorization) {
         const token = Authorization.replace('Bearer ', '');
-        const {userId} = jwt.verify(token, APP_SECRET);
 
+        return jwt.verify(token, APP_SECRET);
+    }
+
+    throw new Error('Not authorized');
+}
+
+function getUserId(context) {
+    const {userId} = getIsAuthorized(context);
+
+    if (userId) {
         return userId;
     }
 
-    throw new Error('Not authenticated');
+    throw new Error('No user id found');
 }
 
 module.exports = {
     APP_SECRET,
-    getUserId
+    getUserId,
+    getIsAuthorized
 };
